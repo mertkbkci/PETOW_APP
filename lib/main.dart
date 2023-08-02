9126208a97bb740580600ee252f5cd2706b81be6
mertkbkci <metrkbkci@gmail.com>
2023-08-02 16:44:06 +0300

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:petow_app/firebase_options.dart';
-import 'package:petow_app/screens/login_screen.dart';
+//import 'package:petow_app/screens/login_screen.dart';
 import 'package:petow_app/screens/page_view.dart';
 
 //import 'layout/camera.dart';
